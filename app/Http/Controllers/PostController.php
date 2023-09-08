e2180e618a796aae4fa748b975f2dacbe50d20c3
Satoshi-Harakawa <hrkw-0902_sts@outlook.jp>
2023-09-09 00:12:30 +0900

    [add]データ取得と表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,6 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-    return view('posts/index') -> with(['posts' => $post -> get()]);    //'posts'は定義した変数、$postsはmysqlのテーブル名、get()は全データを配列に入れる//
-    }                                                                   //withを使えば名前を指定してデータを呼び出せる//
+    return view('posts/index') -> with(['posts' => $post -> getPaginateByLimit()]); 
+    }                                                                   
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,5 +19,9 @@
                 </div>
             @endforeach
         </div>
+        
+        <div class="paginate">
+            {{$posts->links()}}
+        </div>
     </body>
 </html>
