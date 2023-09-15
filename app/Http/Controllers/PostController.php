2afa47988acdf3e7441fcc469be00e58002d4d0d
Satoshi-Harakawa <hrkw-0902_sts@outlook.jp>
2023-09-15 11:25:02 +0900

    [add]ブログ投稿保存機能

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,4 +21,12 @@ class PostController extends Controller
     {
         return view('posts/create');
     }
+    
+    public function store(request $request,post $post)
+    {
+        $input = $request['post'];
+        
+        $post->fill($input)->save();
+        return redirect('/posts/'.$post->id);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+    
 class Post extends Model
 {
     use HasFactory;
@@ -12,4 +12,9 @@ class Post extends Model
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);    
     }
+    
+    protected $fillable = [
+        'title',
+        'body',
+    ];
 }
