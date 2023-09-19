60702c5bc6664a6395f692f1bf06f857c0593ced
Satoshi-Harakawa <hrkw-0902_sts@outlook.jp>
2023-09-19 21:29:36 +0900

    [add]ブログ編集機能

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -8,27 +8,28 @@
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         
     </head>
-    
+
     <body class="antialiased">
-        <h1>Blog Name</h1>
-        <form action="/posts" method="POST">
+        <h1>編集画面</h1>
+        <form action="/posts/{{ $post->id }}" method="POST">
             @csrf
+            @method("PUT")
             <div class="title">
                 <h2>タイトル</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" value="{{old('post.title')}}" />
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ $post->title }}"/>
                 <p class="error_title" style="color:red">{{ $errors -> first('post.title')}}</p>
             </div>
             
             <div class="Body">
                 <h2>本文</h2> 
-                <textarea name="post[body]" placeholder="今日も１日お疲れ様でした。">{{old('post.body')}}</textarea>
+                <textarea name="post[body]" placeholder="今日も１日お疲れ様でした。">{{ $post->body }}</textarea>
                 <p class="error_body" style="color:red">{{ $errors -> first('post.body')}}</p>
             </div>
-            <input type="submit" value="store"/>
+            <input type="submit" value="update"/>
         </form>
         
         <div class="footer">
-            <a href="/">戻る</a>
+            <a href="/posts/{{ $post->id }}">戻る</a>
         </div>
     </body>
 </html>
